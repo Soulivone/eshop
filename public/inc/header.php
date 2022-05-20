e0033d7390cf6b858b5dc99528236f515b31dfb5
Soulivone CHANTANA <69951548+Soulivone@users.noreply.github.com>
2022-05-20 14:30:38 +0700

    Update header.php

--- public/inc/header.php
+++ public/inc/header.php
@@ -18,7 +18,7 @@ require_once 'DB//utf8tolatintutil.php';
 	<!-- Favicon -->
 	<link rel="icon" type="image/png" href="images/favicon.png">
 	<!-- Web Font -->
-	<base href="http://https://soulivone.github.io/eshop">
+	<base href="https://soulivone.github.io/eshop">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
 
 	<!-- StyleSheet -->
